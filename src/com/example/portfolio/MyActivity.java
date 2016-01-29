3e176091f685e87798fa0e30a2d7390323027688
Akshaya Gupta <akshaya@zophop.com>
2016-01-29 10:09:33 +0530

    adding onclicklsitenr to btton

--- src/com/example/portfolio/MyActivity.java
+++ src/com/example/portfolio/MyActivity.java
@@ -1,7 +1,11 @@
 package com.example.portfolio;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MyActivity extends Activity
 {
@@ -10,5 +14,34 @@ public class MyActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        addButtonListeners();
     }
+
+    private void addButtonListeners()
+    {
+        addListener(R.id.super_streamer);
+        addListener(R.id.build_bigger);
+        addListener(R.id.capstone);
+        addListener(R.id.xyz_reader);
+        addListener(R.id.super_duo);
+
+    }
+
+    public void addListener(int viewId)
+    {
+        Button button = ((Button) findViewById(viewId));
+        final String buttonDescription = ((String) button.getText());
+        final Context context = this;
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(context, "This button will launch " + buttonDescription, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 }
